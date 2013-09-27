53c4059c20bc99c3d08d4e81180d55996a1c4f04
Alexander Boyd <alex@opengroove.org>
2013-09-26 20:57:28 -0600

    Restructuring into classes

--- remote_console.py
+++ remote_console.py
@@ -2,12 +2,20 @@
 import code
 import socket
 import threading
+import inspect
+import sys
 
-class Console(code.InteractiveConsole):
-    def __init__(self, s, local_vars=None):
+_former_thread_class = threading.Thread
+_former_stdout = sys.stdout
+_former_stderr = sys.stderr
+_former_stdin = sys.stdin
+
+
+class RemoteConsole(_former_thread_class, code.InteractiveConsole):
+    def __init__(self, connection, local_vars):
+        _former_thread_class.__init__(self)
         code.InteractiveConsole.__init__(self, local_vars)
-        self.socket = s
-        self.connection = s.makefile()
+        self.connection = connection
     
     def write(self, text):
         if self.connection:
@@ -25,25 +33,51 @@ class Console(code.InteractiveConsole):
             print "Telnet console session disconnected: %s" % e
             self.connection.close()
             self.connection = None
-            self.socket.close()
             raise EOFError()
+    
+    def run(self):
+        self.interact()
 
 
-def listen(port, local_vars=None):
-    t = threading.Thread(target=lambda: listen_synchronously(port, local_vars))
-    t.setDaemon(True)
-    t.start()
+class RemoteConsoleServer(_former_thread_class):
+    def __init__(self, port, host="127.0.0.1", local_vars=None):
+        _former_thread_class.__init__(self)
+        if local_vars is None:
+            # Use the locals from the topmost frame. This is the closest I've
+            # come to implicitly using the main console's locals if a dict
+            # isn't explicitly specified.
+            frame = inspect.currentframe()
+            while frame.f_back:
+                frame = frame.f_back
+            local_vars = frame.f_locals
+        self.host = host
+        self.port = port
+        self.local_vars = local_vars
+        self.setDaemon(True)
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((self.host, self.port))
+        self.server.listen(10)
+    
+    def run(self):
+        # TODO: We're basically reimplementing most of socketserver.TCPServer
+        # with socketserver.ThreadingMixIn mixed in; we might as well just
+        # reuse those classes.
+        while True:
+            s, _ = self.server.accept()
+            connection = s.makefile()
+            s.close()
+            self.start_remote_console(connection)
+    
+    def start_remote_console(self, connection):
+        RemoteConsole(connection, self.local_vars).start()
+    
+    def close(self):
+        self.server.close()
 
 
-def listen_synchronously(port, local_vars=None):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server.bind(("127.0.0.1", port))
-    server.listen(10)
-    while True:
-        s, _ = server.accept()
-        console = Console(s, local_vars)
-        threading.Thread(target=console.interact).start()
+def listen(port):
+    RemoteConsoleServer(port).start()
 
 
 
